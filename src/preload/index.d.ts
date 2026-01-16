a1d22ff7f906264a0054757b45afc349cf07173f
thanhloi <marcomoi395@gmail.com>
2026-01-16 09:51:08 +0700

    feat(ui): enhance HTML structure and styling for improved user experience

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,42 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 
+export interface DataImport {
+    type: string
+    payload: {
+        azureKey?: string
+        options: {
+            quiz: boolean
+            flashcard: boolean
+        }
+    }
+}
+
+export interface FileImport extends DataImport {
+    payload: {
+        filePath: string
+    }
+}
+
+export interface NotionSync extends DataImport {
+    payload: {
+        token: string
+    }
+}
+
+export interface DataResponse {
+    status: string
+    message?: string
+    data?: string | object
+}
+
 declare global {
-  interface Window {
-    electron: ElectronAPI
-    api: unknown
-  }
+    interface Window {
+        electron: ElectronAPI & {
+            openFileDialog: () => Promise<{ filePath: string; canceled?: boolean }>
+        }
+        api: {
+            getFilePath: (file: File) => string
+            sendImport: (importData: FileImport | NotionSync) => Promise<DataResponse>
+        }
+    }
 }
