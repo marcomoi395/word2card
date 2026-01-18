31f53da025f3b318c5f19c028db646101032cddf
thanhloi <marcomoi395@gmail.com>
2026-01-18 08:29:27 +0700

    feat(import): enhance import functionality with audio directory initialization

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,15 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { electronApp, is, optimizer } from '@electron-toolkit/utils'
+import { app, BrowserWindow, ipcMain, shell } from 'electron'
+import fs from 'fs'
+import path, { join } from 'path'
 import icon from '../../resources/icon.png?asset'
+import type { FileImport, NotionSync } from '../preload/index.d'
+import { readFileContent } from './helper/readFile'
+import { modelFlashcardParams } from './helper/modelFlashcard'
+import { checkAnkiConnect, sendRequest } from './anki-connect'
+import { createFlashcards, QuizNote } from './handle'
 
 function createWindow(): void {
-    // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 900,
         height: 670,
@@ -27,13 +32,71 @@ function createWindow(): void {
         return { action: 'deny' }
     })
 
-    // HMR for renderer base on electron-vite cli.
-    // Load the remote URL for development or the local html file for production.
     if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
         mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
     } else {
         mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
     }
+}
+
+const init = async (audioDir: string) => {
+    // Check audio folder
+    if (!fs.existsSync(audioDir)) {
+        try {
+            fs.mkdirSync(audioDir, { recursive: true })
+        } catch (err) {
+            console.error('', err)
+        }
+    }
+
+    // Create default models if not exist
+    const modelsResponse = (await sendRequest({
+        action: 'findModelsByName',
+        version: 6,
+        params: {
+            modelNames: ['AnkiVNModel_Flashcard']
+        }
+    })) as { result: (object & { name: string })[]; error: string | null }
+
+    let models: string[] = []
+
+    if (modelsResponse.result !== null) {
+        models = modelsResponse.result.map((i) => i.name)
+    }
+
+    if (models.indexOf('AnkiVNModel_Flashcard') === -1) {
+        try {
+            await sendRequest({
+                action: 'createModel',
+                version: 6,
+                params: modelFlashcardParams
+            })
+        } catch (_err) {
+            return {
+                status: 'error',
+                message: 'Failed to create flashcard model.'
+            }
+        }
+    }
+}
+
+const createDeckIfNotExist = async (deckName: string) => {
+    const decks = (await sendRequest({
+        action: 'deckNames',
+        version: 6
+    })) as { result: string[]; error: string | null }
+
+    if (decks.result.includes(deckName)) {
+        return
+    }
+
+    await sendRequest({
+        action: 'createDeck',
+        version: 6,
+        params: {
+            deck: deckName
+        }
+    })
 }
 
 app.whenReady().then(() => {
@@ -42,18 +105,92 @@ app.whenReady().then(() => {
         optimizer.watchWindowShortcuts(window)
     })
 
-    ipcMain.handle('send-import', async (_, importData) => {
-        console.log('[1]:::', importData)
-        return {
-            status: 'success'
+    ipcMain.handle('send-import', async (_, importData: FileImport | NotionSync) => {
+        if ((await checkAnkiConnect()) === false) {
+            return {
+                status: 'error',
+                message:
+                    'AnkiConnect is not running. Please start Anki and ensure AnkiConnect is installed.'
+            }
+        }
+
+        // Initialize folders and models
+        const pathFile = path.join(app.getPath('userData'))
+        const audioDir = path.join(pathFile, 'audio')
+        void (await init(audioDir))
+
+        let words: string[] | null = []
+        switch (importData.type) {
+            case 'FILE_IMPORT':
+                words = await readFileContent(importData.payload.filePath)
+                break
+
+            case 'NOTION_SYNC':
+                break
+            default:
+                console.error('Unknown import data type')
+        }
+
+        if (words === null) {
+            return {
+                status: 'error',
+                message: 'Failed to read words from the file.'
+            }
+        }
+
+        let isAudio: boolean = false
+        if (importData.payload.azureKey) {
+            isAudio = true
+        }
+
+        // Check existing deck and create if not exist
+        if (importData.payload.deck === '') {
+            importData.payload.deck = `Vocabulary::Imported::${new Date().toISOString().split('T')[0]}`
+        }
+        createDeckIfNotExist(importData.payload.deck)
+
+        const notes: QuizNote[] = []
+
+        if (importData.payload.options.flashcard) {
+            const newNotes: QuizNote[] = await createFlashcards(
+                words,
+                audioDir,
+                importData.payload.deck,
+                isAudio
+            )
+            notes.push(...newNotes)
+        }
+
+        if (notes.length > 0) {
+            const result = (await sendRequest({
+                action: 'addNotes',
+                version: 6,
+                params: {
+                    notes: notes
+                }
+            })) as { result: string | null; error: string }
+
+            if (result.error === null) {
+                return {
+                    status: 'success'
+                }
+            } else {
+                return {
+                    status: 'error',
+                    error: result.error
+                }
+            }
+        } else {
+            return {
+                satus: 'error',
+                message: 'No cards to add.'
+            }
         }
     })
 
     createWindow()
 
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
         }
