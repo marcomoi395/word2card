d72663b4d99359ed68aa2c2b059b57127175940b
thanhloi <marcomoi395@gmail.com>
2026-01-19 15:38:27 +0700

    feat(config): add vite-plugin-static-copy for JSON file handling and update product name

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'electron-vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    main: {},
+    main: {
+        plugins: [
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: 'src/main/helper/model-flashcard.json',
+                        dest: 'helper'
+                    }
+                ]
+            })
+        ]
+    },
     preload: {},
     renderer: {}
 })
