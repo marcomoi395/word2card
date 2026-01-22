b0996d059840512a41dfc0eee92db3668bab5a94
thanhloi <marcomoi395@gmail.com>
2026-01-22 08:31:28 +0700

    feat(notion): update NotionService to retrieve data source and refactor getPages method

--- src/main/notion.ts
+++ src/main/notion.ts
@@ -1,44 +1,5 @@
-import { Client } from '@notionhq/client'
+import { Client, DatabaseObjectResponse } from '@notionhq/client'
 import State from './state'
-
-// interface NotionResponse {
-//     results: Array<{
-//         id: string
-//         properties: {
-//             English: { title: Array<{ plain_text: string }> }
-//             Category: { multi_select: Array<{ name: string }> }
-//             Pronunciation: { rich_text: Array<{ plain_text: string }> }
-//             'Meaning (VN)': { rich_text: Array<{ plain_text: string }> }
-//             Examples: { rich_text: Array<{ plain_text: string }> }
-//             [key: string]: any
-//         }
-//     }>
-// }
-
-// const mapNotionResponseToFlashcards = (data: NotionResponse): Flashcard[] => {
-//     if (!data || !data.results) {
-//         return []
-//     }
-//
-//     return data.results.map((page) => {
-//         const props = page.properties
-//
-//         const getRichText = (prop: any) => prop?.rich_text?.[0]?.plain_text
-//         const getTitle = (prop: any) => prop?.title?.[0]?.plain_text
-//         const getSelect = (prop: any) => prop?.multi_select?.[0]?.name
-//
-//         return {
-//             id: page.id,
-//             word: getTitle(props.English) || '',
-//             pos: getSelect(props.Category),
-//             example: getRichText(props.Examples),
-//             vietnamese: getRichText(props['Meaning (VN)']) || '',
-//             ipa: getRichText(props.Pronunciation),
-//             image: undefined,
-//             audio_word: undefined
-//         }
-//     })
-// }
 
 export class NotionService {
     private static instance: Client | null = null
@@ -66,8 +27,27 @@ export class NotionService {
         return NotionService.instance
     }
 
-    public static async getPages(dataSourceId: string) {
+    public static async retrieveDataSource(databaseId: string) {
         const notion = NotionService.getInstance()
+
+        const database = (await notion.databases.retrieve({
+            database_id: databaseId
+        })) as DatabaseObjectResponse
+
+        if (database.data_sources && database.data_sources.length > 0) {
+            return database.data_sources[0].id
+        }
+
+        return null
+    }
+
+    public static async getPages(databaseId: string) {
+        const notion = NotionService.getInstance()
+
+        const dataSourceId = await NotionService.retrieveDataSource(databaseId)
+        if (!dataSourceId) {
+            throw new Error('No data source found for the given database ID')
+        }
 
         return await notion.dataSources.query({
             data_source_id: dataSourceId
