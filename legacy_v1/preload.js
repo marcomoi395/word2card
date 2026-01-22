dc893719b1ce0df88e1eb49aa1bd4e7d237e76e6
thanhloi <marcomoi395@gmail.com>
2026-01-22 16:45:06 +0700

    chore: cleanup unused source file

--- legacy_v1/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
-    ipcRenderer: {
-        send: (channel, data) => ipcRenderer.send(channel, data),
-    },
-});
