7ae2f4a55dc426ec715ad0df34514528608603c5
thanhloi <marcomoi395@gmail.com>
2026-01-24 21:01:34 +0700

    feat(readFile): enhance file reading by trimming lines and filtering empty entries

--- src/main/helper/readFile.ts
+++ src/main/helper/readFile.ts
@@ -3,7 +3,10 @@ import fs from 'fs'
 export const readFileContent = async (path: string) => {
     try {
         const res = await fs.promises.readFile(path, 'utf-8')
-        return res.split('\n')
+        return res
+            .split('\n')
+            .map((line) => line.trim())
+            .filter((line) => line.length > 0)
     } catch (e) {
         console.error('Error reading file:', e)
         return null
